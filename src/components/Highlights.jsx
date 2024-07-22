1a6a3e8613a47349878f9666f9de8bd8431d9874
Jay-Rivera <119644467+Jay-Rivera@users.noreply.github.com>
2024-07-21 23:52:56 -0700

    Fixed bugs

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import Highlight from "./UI/Highlight";
-import { icon } from "@fortawesome/fontawesome-svg-core";
 
 const Highlights = () => {
   return (
